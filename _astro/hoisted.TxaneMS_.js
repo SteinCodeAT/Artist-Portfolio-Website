9ea6a41cb3e6f73adee5769f8946dd6dd69fe8d7
kalvinter <kalvinter@users.noreply.github.com>
2024-02-18 12:09:30 +0000

    deploy: 1835c97f22bc08d81e6b25ab32607489dfaaedf3

---
+++ _astro/hoisted.TxaneMS_.js
@@ -0,0 +1,1 @@
+import"./hoisted._hoC4l98.js";
---
+++ _astro/hoisted._hoC4l98.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",a=>{document.querySelectorAll(".image-wrapper").forEach(e=>{e.querySelectorAll("img").length>0&&e.classList.add("has-images")})});
