c3c20be6682dc292e891ccfee24f398471bd0583
Kalvinter <web@blue-dots.at>
2024-03-11 21:24:28 +0100

    added dots animation to navbar

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,4 @@
-function randomPosition(maxDistancePercent) {
+function randomPositionInCircle(maxDistancePercent) {
     // Get a random angle
     var angle = Math.random() * Math.PI * 2;
     // Get a random distance within maxDistancePercent
@@ -9,10 +9,19 @@ function randomPosition(maxDistancePercent) {
     return { x: x, y: y };
   }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const container = document.querySelector('.dots-containter');
-    const numOfDots = 170; // Change this number based on how many dots you want
-  
+function randomPositionInSquare(maxDistancePercent){
+    let xDistance = maxDistancePercent * Math.random()
+    let yDistance = maxDistancePercent * Math.random()
+
+    xDistance = (Math.random() > 0.5) ? xDistance * -1 : xDistance
+    yDistance = (Math.random() > 0.5) ? yDistance * -1 : yDistance
+
+    return { x: xDistance, y: yDistance };
+}
+
+function generateDots(containerClassName, numOfDots, getRandomPositionFunc, maxDistancePercent){
+    const container = document.querySelector(`.${containerClassName}`);
+    
     for (var i = 0; i < numOfDots; i++) {
       let dot = document.createElement('div');
       dot.classList.add('dot');
@@ -21,7 +30,7 @@ document.addEventListener("DOMContentLoaded", function() {
       let opacity = Math.random(); // Opacity between 0 and 1
       // Get a random position within a max distance from center
 
-        let position = randomPosition(45); // Max distance from center in percent
+        let position = getRandomPositionFunc(maxDistancePercent); // Max distance from center in percent
         let posX = 50 + position.x; // Center is 50% for left
         let posY = 50 + position.y; // Center is 50% for top
 
@@ -39,6 +48,41 @@ document.addEventListener("DOMContentLoaded", function() {
     
       container.appendChild(dot);
     }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    generateDots("circle-dots-container", 170, randomPositionInCircle, 45)
+    generateDots("banner-dots-container", 20, randomPositionInSquare, 50)
+    /* 
+    const container = document.querySelector('.dots-container');
+    const numOfDots = 170; // Change this number based on how many dots you want
+  
+    for (var i = 0; i < numOfDots; i++) {
+      let dot = document.createElement('div');
+      dot.classList.add('dot');
+      
+      let size = Math.random() * 5 + 8; // Size between 2px and 7px
+      let opacity = Math.random(); // Opacity between 0 and 1
+      // Get a random position within a max distance from center
+
+        let position = randomCirclePosition(45); // Max distance from center in percent
+        let posX = 50 + position.x; // Center is 50% for left
+        let posY = 50 + position.y; // Center is 50% for top
+
+      // Set random animation delays and durations for the fading effect
+      let animationDelay = Math.random() * 5; // up to 5 seconds delay
+      let animationDuration = 3 + Math.random() * 5; // between 3 and 8 seconds long
+
+      dot.style.width = `${size}px`;
+      dot.style.height = `${size}px`;
+      dot.style.opacity = opacity;
+      dot.style.left = `${posX}%`;
+      dot.style.top = `${posY}%`;
+      dot.style.animation = `fadeEffect ${animationDuration}s infinite ${animationDelay}s`;
+  
+    
+      container.appendChild(dot);
+    }*/
   });
 
 // LINES 
